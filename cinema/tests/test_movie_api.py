c197af62658954f3e53192583884942c791c0b3c
MorsImmortalis05 <morsimmortalis05@gmail.com>
2025-09-15 01:16:48 +0300

    sol

--- cinema/tests/test_movie_api.py
+++ cinema/tests/test_movie_api.py
@@ -9,7 +9,13 @@ from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
 
-from cinema.models import Movie, MovieSession, CinemaHall, Genre, Actor
+from cinema.models import (
+    Movie,
+    MovieSession,
+    CinemaHall,
+    Genre,
+    Actor
+)
 
 MOVIE_URL = reverse("cinema:movie-list")
 MOVIE_SESSION_URL = reverse("cinema:moviesession-list")
